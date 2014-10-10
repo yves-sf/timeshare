795ba7a70350f429224fd39ec66268a53fed939e
Yves Lucas <ylucas@ylucas.com>
2014-10-10 11:16:16 -0700

    confirmations controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get  'confirmations/new'
 
   get  'sessions/new'
   post 'sessions/create'
---
+++ spec/views/confirmations/create.html.slim_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "confirmations/new.html.slim", :type => :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
