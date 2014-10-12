18064e52264e6a81fb84ec9add0d1ce5883cbf3a
Yves Lucas <ylucas@ylucas.com>
2014-10-11 22:37:23 -0700

    Reservations controller and views

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,21 @@
+class ReservationsController < InheritedResources::Base
+  respond_to :html
+
+  def new
+    @reservation = Reservation.new
+    @reservation.add_dynamic_fields @reservation.amenities, Boolean
+  end
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+
+    # todo send email to tour
+    redirect_to @reservation
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit([:preferred_housing_date, :additional_questions].concat(Reservation.amenities))
+  end
+
+end
