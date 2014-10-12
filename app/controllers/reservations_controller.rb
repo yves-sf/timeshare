7873a98cc10bc42f6c63a16bdf3f83ff6fd0c76d
Yves Lucas <ylucas@ylucas.com>
2014-10-12 11:29:40 -0700

    pass host and cosmetic to email views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,7 +8,7 @@ class ReservationsController < InheritedResources::Base
 
   def create
     @reservation = current_user.reservations.create(reservation_params)
-    send_confirmation_emails @reservation, current_user, request.ip
+    send_confirmation_emails @reservation, current_user, request
     redirect_to @reservation
   end
 
@@ -17,9 +17,9 @@ class ReservationsController < InheritedResources::Base
     params.require(:reservation).permit([:preferred_housing_date, :additional_questions].concat(Reservation.amenities))
   end
 
-  def send_confirmation_emails(reservation, current_user, ip_address)
-    UserMailer.confirmation_reservation(reservation, current_user).deliver
-    OfficeMailer.desk_resa_request(reservation, current_user, ip_address).deliver
+  def send_confirmation_emails(reservation, current_user, request)
+    UserMailer.confirmation_reservation(reservation, current_user, request.host).deliver
+    OfficeMailer.desk_resa_request(reservation, current_user, request.ip).deliver
   end
 
 end
