394cf9b67d1c3c12418d27938365204b1c59fd80
Yves Lucas <ylucas@ylucas.com>
2014-10-12 10:54:57 -0700

    add links to resa confirmation email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,11 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: "Please confirm your email ")
   end
 
-  def confirmation_reservation(reservation, user)
+  def confirmation_reservation(reservation, user, host)
     @reservation = reservation
-    @user = user
+    @user        = user
+    @url_resa    = "http://#{host}/reservations/#{@reservation.id}"
+    @init_url    = "http://#{host}/users/#{@user.token}/token"
     mail(to: @user.email, subject: "Reservation request confirmation ")
   end
 end
